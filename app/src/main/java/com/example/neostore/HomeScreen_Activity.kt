186fb735e76de43eb5c30beace15fbe44b55dda6
vinita patil <vinita.patil@neosofttech.com>
2019-06-03 14:48:00 +0530

    fisrt commit

---
+++ app/src/main/java/com/example/neostore/HomeScreen_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.neostore
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreen_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen_)
+    }
+}
