b352ee391b870b77123d98d88f38be915c474116
vinita patil <vinita.patil@neosofttech.com>
2019-06-04 14:47:56 +0530

    file name change

--- app/src/main/java/com/example/neostore/HomeScreenActivity.kt
+++ app/src/main/java/com/example/neostore/HomeScreenActivity.kt
@@ -2,11 +2,26 @@ package com.example.neostore
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.view.SupportActionModeWrapper
+import android.support.v7.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_home_screen.*
 
 class HomeScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+      /* setSupportActionBar(toolbar)
+
+        val actionBar = supportActionBar
+
+        actionBar!!.title ="NeoStore"
+
+        actionBar.setDisplayShowHomeEnabled(true)
+        actionBar.setLogo(R.mipmap.ic_launcher)
+        actionBar.setDisplayUseLogoEnabled(true)*/
+
+
     }
 }
