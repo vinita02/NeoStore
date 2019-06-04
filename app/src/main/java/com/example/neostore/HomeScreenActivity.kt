053c10522867d81b862bb59b002e13b12775b862
vinita patil <vinita.patil@neosofttech.com>
2019-06-04 15:35:50 +0530

    home screen

--- app/src/main/java/com/example/neostore/HomeScreenActivity.kt
+++ app/src/main/java/com/example/neostore/HomeScreenActivity.kt
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.view.SupportActionModeWrapper
 import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_home_screen.*
 
 class HomeScreenActivity : AppCompatActivity() {
@@ -11,7 +13,7 @@ class HomeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
-
+    }
       /* setSupportActionBar(toolbar)
 
         val actionBar = supportActionBar
@@ -22,6 +24,18 @@ class HomeScreenActivity : AppCompatActivity() {
         actionBar.setLogo(R.mipmap.ic_launcher)
         actionBar.setDisplayUseLogoEnabled(true)*/
 
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.menu,menu)
+            return true
+        }
 
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemid)
+        {
+
+        }
+        return super.onContextItemSelected(item)
     }
+
+
 }
